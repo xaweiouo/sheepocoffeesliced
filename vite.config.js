ab22183106396d7ec7f942efacbfe7470c99846c
awei <xaweiouo@gmail.com>
2025-10-09 12:07:58 +0800

    footer新增僅供求職使用

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
   // 發佈到git時要加入  /coffee_demo/
-  base: '/coffee/',
+  base: '/sheepocoffeesliced/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
